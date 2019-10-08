8d63441e87e24b2d7e7a092ac43de8528679a326
Tiffany <>
2019-10-08 11:06:25 +0200

    Smile prob on camera pics and mood guess

--- moody/lib/views/home_page.dart
+++ moody/lib/views/home_page.dart
@@ -18,7 +18,6 @@ class _HomePageState extends State<HomePage> {
   List<Widget> fabButtons;
   GlobalKey<CircleFloatingButtonState> key = GlobalKey<CircleFloatingButtonState>();
 
-//  File _image;
   File _file;
 
   List<VisionFace> _face = <VisionFace>[];
@@ -26,20 +25,6 @@ class _HomePageState extends State<HomePage> {
   VisionFaceDetectorOptions options = new VisionFaceDetectorOptions(modeType: VisionFaceDetectorMode.Accurate, landmarkType: VisionFaceDetectorLandmark.All, classificationType: VisionFaceDetectorClassification.All, minFaceSize: 0.15, isTrackingEnabled: true);
 
   FirebaseVisionFaceDetector detector = FirebaseVisionFaceDetector.instance;
-
-//  Future getImageFromCam() async {
-//    var image = await ImagePicker.pickImage(source: ImageSource.camera);
-//    setState(() {
-//      _image = image;
-//    });
-//  }
-//
-//  Future getImageFromGallery() async {
-//    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-//    setState(() {
-//      _image = image;
-//    });
-//  }
 
   @override
   void initState() {
@@ -49,7 +34,6 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.amberAccent,
           child: Icon(Icons.camera_alt, color: Colors.pink[900]),
           tooltip: "Camera",
-//        onPressed: getImageFromCam,
           onPressed: () async {
             var file = await ImagePicker.pickImage(source: ImageSource.camera);
             setState(
@@ -72,7 +56,6 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.amberAccent,
           tooltip: "Gallery",
           child: Icon(Icons.image, color: Colors.pink[900]),
-//        onPressed: getImageFromGallery,
           onPressed: () async {
             var file = await ImagePicker.pickImage(source: ImageSource.gallery);
             setState(
@@ -94,30 +77,22 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
-//  Widget showBody(File file) {
-//    return new Container(
-//      child: new Stack(
-//        children: <Widget>[
-////          _buildImage(),
-//          _showDetails(_face),
-//        ],
-//      ),
-//    );
-//  }
-
-  Widget _buildImage() {
-    return new SizedBox(
-      height: 300.0,
-      child: new Center(
+  _buildImage() {
+    return SizedBox(
+      height: 200.0,
+      child: Center(
         child: _file == null
-            ? new Text('Select image using floating button...', style: TextStyle(color: Colors.amberAccent),)
-            : new FutureBuilder<Size>(
+            ? Text(
+                'Please use the button at the bottom to pick an image',
+                style: TextStyle(color: Colors.amberAccent),
+              )
+            : FutureBuilder<Size>(
                 future: _getImageSize(Image.file(_file, fit: BoxFit.fitWidth)),
                 builder: (BuildContext context, AsyncSnapshot<Size> snapshot) {
                   if (snapshot.hasData) {
-                    return Container(foregroundDecoration: TextDetectDecoration(_face, snapshot.data), child: Image.file(_file, fit: BoxFit.fitWidth));
+                    return Container(child: Image.file(_file, fit: BoxFit.fitWidth));
                   } else {
-                    return new Text('Please wait...');
+                    return Text('Please wait...');
                   }
                 },
               ),
@@ -137,22 +112,9 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(bottom: 30.0),
+            padding: const EdgeInsets.only(bottom: 15.0),
             child: Text("Moody", style: TextStyle(color: Colors.amberAccent, fontSize: 45.0, fontFamily: 'QuicksandB')),
           ),
-//          Padding(
-//            padding: EdgeInsets.only(left: 8.0, right: 8.0),
-//            child: AspectRatio(
-//              aspectRatio: 1 / 1,
-//              child: Container(
-//                alignment: Alignment.center,
-//                height: 300,
-//                width: 300,
-//                color: Colors.teal,
-//                child: _image == null ? Text('No image selected.') : Image.file(_image),
-//              ),
-//            ),
-//          ),
           Padding(
             padding: EdgeInsets.only(left: 8.0, right: 8.0),
             child: AspectRatio(
@@ -161,6 +123,7 @@ class _HomePageState extends State<HomePage> {
             ),
           ),
           _showDetails(_face),
+          _moodGuess(_face),
         ],
       ),
     );
@@ -198,118 +161,29 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class TextDetectDecoration extends Decoration {
-  final Size _originalImageSize;
-  final List<VisionFace> _texts;
-
-  TextDetectDecoration(List<VisionFace> texts, Size originalImageSize)
-      : _texts = texts,
-        _originalImageSize = originalImageSize;
-
-  @override
-  BoxPainter createBoxPainter([VoidCallback onChanged]) {
-    return new _TextDetectPainter(_texts, _originalImageSize);
-  }
-}
-
 Future _getImageSize(Image image) {
   Completer<Size> completer = new Completer<Size>();
-//  image.image.resolve(new ImageConfiguration()).addListener(
-//          (ImageInfo info, bool _) => completer.complete(
-//          Size(info.image.width.toDouble(), info.image.height.toDouble())));
   image.image.resolve(ImageConfiguration()).addListener(ImageStreamListener((ImageInfo info, bool _) => completer.complete(Size(info.image.width.toDouble(), info.image.height.toDouble()))));
   return completer.future;
 }
 
-Widget _showDetails(List<VisionFace> faceList) {
+_showDetails(List<VisionFace> faceList) {
   if (faceList == null || faceList.length == 0) {
-    return new Text('', textAlign: TextAlign.center);
+    return Text('', textAlign: TextAlign.center);
   }
-  return new Container(
+  return Container(
     child: Expanded(
-      child: new ListView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: faceList.length,
-        itemBuilder: (context, i) {
-          checkData(faceList);
-          return _buildRow(faceList[0].hasLeftEyeOpenProbability, faceList[0].headEulerAngleY, faceList[0].headEulerAngleZ, faceList[0].leftEyeOpenProbability, faceList[0].rightEyeOpenProbability, faceList[0].smilingProbability, faceList[0].trackingID);
-        },
-      ),
+      child: Text("Smile probability: ${faceList[0].smilingProbability}"),
     ),
   );
 }
 
-//For checking and printing different variables from Firebase
-void checkData(List<VisionFace> faceList) {
-  final double uncomputedProb = -1.0;
-  final int uncompProb = -1;
-
-  for (int i = 0; i < faceList.length; i++) {
-    Rect bounds = faceList[i].rect;
-    print('Rectangle : $bounds');
-
-    VisionFaceLandmark landmark = faceList[i].getLandmark(FaceLandmarkType.LeftEar);
-
-    if (landmark != null) {
-      VisionPoint leftEarPos = landmark.position;
-      print('Left Ear Pos : $leftEarPos');
-    }
-
-    if (faceList[i].smilingProbability != uncomputedProb) {
-      double smileProb = faceList[i].smilingProbability;
-      print('Smile Prob : $smileProb');
-    }
-
-    if (faceList[i].rightEyeOpenProbability != uncomputedProb) {
-      double rightEyeOpenProb = faceList[i].rightEyeOpenProbability;
-      print('RightEye Open Prob : $rightEyeOpenProb');
-    }
-
-    if (faceList[i].trackingID != uncompProb) {
-      int tID = faceList[i].trackingID;
-      print('Tracking ID : $tID');
-    }
-  }
-}
-
-/*
-    HeadEulerY : Head is rotated to right by headEulerY degrees
-    HeadEulerZ : Head is tilted sideways by headEulerZ degrees
-    LeftEyeOpenProbability : left Eye Open Probability
-    RightEyeOpenProbability : right Eye Open Probability
-    SmilingProbability : Smiling probability
-    Tracking ID : If face tracking is enabled
-  */
-Widget _buildRow(bool leftEyeProb, double headEulerY, double headEulerZ, double leftEyeOpenProbability, double rightEyeOpenProbability, double smileProb, int tID) {
-  return ListTile(
-    title: new Text(
-      "\nLeftEyeProb: $leftEyeProb \nHeadEulerY : $headEulerY \nHeadEulerZ : $headEulerZ \nLeftEyeOpenProbability : $leftEyeOpenProbability \nRightEyeOpenProbability : $rightEyeOpenProbability \nSmileProb : $smileProb \nFaceTrackingEnabled : $tID",
-    ),
-    dense: true,
-  );
-}
-
-class _TextDetectPainter extends BoxPainter {
-  final List<VisionFace> _faceLabels;
-  final Size _originalImageSize;
-
-  _TextDetectPainter(faceLabels, originalImageSize)
-      : _faceLabels = faceLabels,
-        _originalImageSize = originalImageSize;
-
-  @override
-  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
-    final paint = new Paint()
-      ..strokeWidth = 2.0
-      ..color = Colors.red
-      ..style = PaintingStyle.stroke;
-
-    final _heightRatio = _originalImageSize.height / configuration.size.height;
-    final _widthRatio = _originalImageSize.width / configuration.size.width;
-    for (var faceLabel in _faceLabels) {
-      final _rect = Rect.fromLTRB(offset.dx + faceLabel.rect.left / _widthRatio, offset.dy + faceLabel.rect.top / _heightRatio, offset.dx + faceLabel.rect.right / _widthRatio, offset.dy + faceLabel.rect.bottom / _heightRatio);
-
-      canvas.drawRect(_rect, paint);
-    }
+_moodGuess(List<VisionFace> faceList) {
+  if (faceList == null || faceList.length == 0) {
+    return Text('', textAlign: TextAlign.center);
+  } else if (faceList[0].smilingProbability >= 0.5) {
+    return Text("Mood: Happy");
+  } else if (faceList[0].smilingProbability < 0.5) {
+    return Text("Mood: Sad");
   }
 }
